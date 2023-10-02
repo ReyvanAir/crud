1bb6d5e53a160b4e21d838300c76c49b878b0f39
GilbyKoloay <lolwtf10001@gmail.com>
2023-10-02 15:45:34 +0800

    modified html structure and URI

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -20,7 +20,7 @@ export default function User() {
   // ambe data dummy
   useEffect(() => {
     (async () => {
-      const req = await fetch('https://dummyjson.com/users');
+      const req = await fetch('https://dummyjson.com/users?limit=50');
       const res = await req.json();
       if (res) {
         setUsers(res.users.map(user => ({
@@ -49,70 +49,72 @@ export default function User() {
     <main>
       <div className='text-2xl'>USER</div>
 
-      <div className='mt-4'>
-        <TextField
-          label='Search User'
-          variant='outlined'
-          size='small'
-          value={searchUserQuery}
-          onChange={e => setSearchUserQuery(e.target.value)}
-        />
-      </div>
-
       <div className='mt-4 overflow-auto'>
         {!users ? (
           <div className='text-center text-lg'>Loading data ...</div>
         ) : (users.length === 0) ? (
           <div className='text-center text-lg'>Data is empty</div>
         ) : (
-          <table className='w-full select-text'>
-            <thead className='bg-tertiary text-neutral-100 border border-tertiary'>
-              <tr>
-                <th className='py-2 px-2'>UID</th>
-                <th className='py-2 px-2'>UserName</th>
-                <th className='py-2 px-2'>Email</th>
-                <th className='py-2 px-2'>Battle</th>
-                <th className='py-2 px-2'>SCORE</th>
-                <th className='py-2 px-2'>W/L Ratio</th>
-                <th className='py-2 px-2'>Action</th>
-              </tr>
-            </thead>
-            <tbody style={{backgroundColor: '#FDF3D3'}}>
-              {filteredUsers().map((user, index) => (
-                <tr key={index}>
-                  <td className='px-2 border border-tertiary'>{user.UID}</td>
-                  <td className='px-2 border border-tertiary'>{user.userName}</td>
-                  <td className='px-2 border border-tertiary'>{user.email}</td>
-                  <td className='px-2 border border-tertiary'>{user.battle}</td>
-                  <td className='px-2 border border-tertiary'>{user.score}</td>
-                  <td className='px-2 border border-tertiary'>{user.WLratio}</td>
-                  <td className='px-2 border border-tertiary text-center'>
-                    <Button
-                      variant='text'
-                      size='small'
-                      onClick={() => setIsDeleteUserDialogOpen(user)}
-                    >
-                      <Delete color='error' />
-                    </Button>
-                    <Button
-                      variant='text'
-                      size='small'
-                      onClick={() => setIsEditUserDialogOpen(user)}
-                    >
-                      <Edit color='warning' />
-                    </Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
+          <>
+            <div className='flex items-center gap-4'>
+              <TextField
+                label='Search User'
+                variant='outlined'
+                size='small'
+                value={searchUserQuery}
+                onChange={e => setSearchUserQuery(e.target.value)}
+              />
+            </div>
 
-      <div className='mt-4 flex justify-end'>
-        <Button variant='contained' onClick={() => setIsAddUserDialogOpen(true)}>
-          Add User
-        </Button>
+            <table className='mt-4 w-full select-text'>
+              <thead className='bg-tertiary text-neutral-100 border border-tertiary'>
+                <tr>
+                  <th className='py-2 px-2'>UID</th>
+                  <th className='py-2 px-2'>UserName</th>
+                  <th className='py-2 px-2'>Email</th>
+                  <th className='py-2 px-2'>Battle</th>
+                  <th className='py-2 px-2'>SCORE</th>
+                  <th className='py-2 px-2'>W/L Ratio</th>
+                  <th className='py-2 px-2'>Action</th>
+                </tr>
+              </thead>
+              <tbody style={{backgroundColor: '#FDF3D3'}}>
+                {filteredUsers().map((user, index) => (
+                  <tr key={index}>
+                    <td className='px-2 border border-tertiary'>{user.UID}</td>
+                    <td className='px-2 border border-tertiary'>{user.userName}</td>
+                    <td className='px-2 border border-tertiary'>{user.email}</td>
+                    <td className='px-2 border border-tertiary'>{user.battle}</td>
+                    <td className='px-2 border border-tertiary'>{user.score}</td>
+                    <td className='px-2 border border-tertiary'>{user.WLratio}</td>
+                    <td className='px-2 border border-tertiary text-center'>
+                      <Button
+                        variant='text'
+                        size='small'
+                        onClick={() => setIsDeleteUserDialogOpen(user)}
+                      >
+                        <Delete color='error' />
+                      </Button>
+                      <Button
+                        variant='text'
+                        size='small'
+                        onClick={() => setIsEditUserDialogOpen(user)}
+                      >
+                        <Edit color='warning' />
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <div className='mt-4 flex justify-end'>
+              <Button variant='contained' onClick={() => setIsAddUserDialogOpen(true)}>
+                Add User
+              </Button>
+            </div>
+          </>
+        )}
       </div>
 
 
