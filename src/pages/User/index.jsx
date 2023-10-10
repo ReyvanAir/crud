c25b4b3516aec9f9e3b11b3e0a824a12f648b33d
ReyvanAir <reyvansolis2402@gmail.com>
2023-10-10 09:16:33 +0800

    tablechange

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,134 +1,180 @@
-import { Delete, Edit } from '@mui/icons-material';
-import { TextField, Button } from '@mui/material';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Delete, Edit } from "@mui/icons-material";
+import {
+  TextField,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TablePagination,
+} from "@mui/material";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import AddUserDialog from './AddUserDialog';
-import DeleteUserDialog from './DeleteUserDialog';
-import EditUserDialog from './EditUserDialog';
-
-
+import AddUserDialog from "./AddUserDialog";
+import DeleteUserDialog from "./DeleteUserDialog";
+import EditUserDialog from "./EditUserDialog";
 
 export default function User() {
   const navigate = useNavigate();
 
-  const [filterUser, setFilterUser] = useState('');
+  const [filterUser, setFilterUser] = useState("");
   const [users, setUsers] = useState(null);
   const [isAddUserDialogOpen, setIsAddUserDialogOpen] = useState(false);
   const [isEditUserDialogOpen, setIsEditUserDialogOpen] = useState(false);
   const [isDeleteUserDialogOpen, setIsDeleteUserDialogOpen] = useState(false);
-
-
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   // ambe data dummy
   useEffect(() => {
     (async () => {
-      const req = await fetch('https://dummyjson.com/users?limit=50');
+      const req = await fetch("https://dummyjson.com/users?limit=50");
       const res = await req.json();
       if (res) {
-        setUsers(res.users.map(user => ({
-          UID: user.id,
-          userName: user.username,
-          email: user.email,
-          battle: user.maidenName,
-          score: user.height,
-          WLratio: user.weight
-        })));
+        setUsers(
+          res.users.map((user) => ({
+            UID: user.id,
+            userName: user.username,
+            email: user.email,
+            battle: user.maidenName,
+            score: user.height,
+            WLratio: user.weight,
+          }))
+        );
       }
     })();
   }, []);
 
-
-
   function filteredUsers() {
-    return users?.filter(user => (
-      (!user || user.userName.includes(filterUser))
-    ));
+    return users?.filter((user) => !user || user.userName.includes(filterUser));
   }
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
-    <main className='flex-1 flex flex-col gap-4 p-4'>
-      <div className='text-2xl'>USER</div>
-      
+    <main className="flex-1 flex flex-col gap-4 p-4">
+      <div className="text-2xl">USER</div>
+
       {!users ? (
-        <div className='text-center text-lg'>Loading data ...</div>
-      ) : (users.length === 0) ? (
-        <div className='text-center text-lg'>Data is empty</div>
+        <div className="text-center text-lg">Loading data ...</div>
+      ) : users.length === 0 ? (
+        <div className="text-center text-lg">Data is empty</div>
       ) : (
         <>
-          <div className='flex gap-4 items-center'>
+          <div className="flex gap-4 items-center">
             <TextField
-              label='Search User'
-              variant='outlined'
-              size='small'
+              label="Search User"
+              variant="outlined"
+              size="small"
               value={filterUser}
-              onChange={e => setFilterUser(e.target.value)}
+              onChange={(e) => setFilterUser(e.target.value)}
             />
           </div>
 
-          <div className='flex-1 overflow-auto'>
-            <table className='w-full'>
-              <thead className='bg-tertiary text-neutral-100 border border-tertiary'>
-                <tr>
-                  <th className='py-2 px-2'>UID</th>
-                  <th className='py-2 px-2'>UserName</th>
-                  <th className='py-2 px-2'>Email</th>
-                  <th className='py-2 px-2'>Battle</th>
-                  <th className='py-2 px-2'>SCORE</th>
-                  <th className='py-2 px-2'>W/L Ratio</th>
-                  <th className='py-2 px-2'>Action</th>
-                </tr>
-              </thead>
-              <tbody className='border border-tertiary' style={{backgroundColor: '#FDF3D3'}}>
-                {filteredUsers().map((user, index) => (
-                  <tr
-                    key={index}
-                    className='hover:cursor-pointer hover:bg-secondary'
-                    onClick={() => navigate(`/user/detail/${user.UID}`)}
-                  >
-                    <td className='px-2 border-y border-tertiary'>{user.UID}</td>
-                    <td className='px-2 border-y border-tertiary'>{user.userName}</td>
-                    <td className='px-2 border-y border-tertiary'>{user.email}</td>
-                    <td className='px-2 border-y border-tertiary'>{user.battle}</td>
-                    <td className='px-2 border-y border-tertiary'>{user.score}</td>
-                    <td className='px-2 border-y border-tertiary'>{user.WLratio}</td>
-                    <td className='px-2 border-y border-tertiary text-center'>
-                      <Button
-                        variant='text'
-                        size='small'
-                        onClick={() => setIsDeleteUserDialogOpen(user)}
+          <div className="flex-1 overflow-auto">
+            <Table className="w-full">
+              <TableHead className="bg-tertiary text-neutral-100 border border-tertiary">
+                <TableRow>
+                  <TableCell className="py-2 px-2">UID</TableCell>
+                  <TableCell className="py-2 px-2">UserName</TableCell>
+                  <TableCell className="py-2 px-2">Email</TableCell>
+                  <TableCell className="py-2 px-2">Battle</TableCell>
+                  <TableCell className="py-2 px-2">SCORE</TableCell>
+                  <TableCell className="py-2 px-2">W/L Ratio</TableCell>
+                  <TableCell className="py-2 px-2">Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <tbody
+                className="border border-tertiary"
+                style={{ backgroundColor: "#FDF3D3" }}
+              >
+                {filteredUsers()
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((user, index) => (
+                    <TableRow
+                    // key={index}
+                    // className="hover:cursor-pointer hover:bg-secondary"
+                    // onClick={() => navigate(`/user/detail/${user.UID}`)}
+                    >
+                      <TableCell className="px-2 border-y border-tertiary  ">
+                        {user.UID}
+                      </TableCell>
+                      <TableCell
+                        key={index}
+                        className="px-2 border-y border-tertiary 
+                      hover:cursor-pointer hover:bg-secondary"
+                        onClick={() => navigate(`/user/detail/${user.UID}`)}
                       >
-                        <Delete color='error' />
-                      </Button>
-                      <Button
-                        variant='text'
-                        size='small'
-                        onClick={() => setIsEditUserDialogOpen(user)}
-                      >
-                        <Edit color='warning' />
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
+                        {user.userName}
+                      </TableCell>
+                      <TableCell className="px-2 border-y border-tertiary">
+                        {user.email}
+                      </TableCell>
+                      <TableCell className="px-2 border-y border-tertiary">
+                        {user.battle}
+                      </TableCell>
+                      <TableCell className="px-2 border-y border-tertiary">
+                        {user.score}
+                      </TableCell>
+                      <TableCell className="px-2 border-y border-tertiary">
+                        {user.WLratio}
+                      </TableCell>
+                      <TableCell className="px-2 border-y border-tertiary text-center">
+                        <Button
+                          variant="text"
+                          size="small"
+                          onClick={() => setIsDeleteUserDialogOpen(user)}
+                        >
+                          <Delete color="error" />
+                        </Button>
+                        <Button
+                          variant="text"
+                          size="small"
+                          onClick={() => setIsEditUserDialogOpen(user)}
+                        >
+                          <Edit color="warning" />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </tbody>
-            </table>
+            </Table>
+            <TablePagination
+              rowsPerPageOptions={[20, 10, 30]}
+              component={"div"}
+              count={filteredUsers().length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
           </div>
 
-          <div className='flex justify-end gap-4'>
-            <Button variant='contained' onClick={() => setIsAddUserDialogOpen(true)}>
+          <div className="flex justify-end gap-4">
+            <Button
+              variant="contained"
+              onClick={() => setIsAddUserDialogOpen(true)}
+            >
               Add User
             </Button>
           </div>
         </>
       )}
 
-
-
       {/* Add User Dialog */}
       {isAddUserDialogOpen && (
-        <AddUserDialog open={isAddUserDialogOpen ? true : false} onClose={() => setIsAddUserDialogOpen(false)} />
+        <AddUserDialog
+          open={isAddUserDialogOpen ? true : false}
+          onClose={() => setIsAddUserDialogOpen(false)}
+        />
       )}
 
       {/* Delete User Dialog */}
@@ -150,4 +196,4 @@ export default function User() {
       )}
     </main>
   );
-};
+}
